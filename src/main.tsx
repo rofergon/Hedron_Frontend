f7fec52d72d596a1793cee89087ca03b7ab63185
rofergon <sebas.2023@gmail.com>
2025-07-24 20:33:00 -0500

    Refactor main.tsx to simplify rendering by removing WagmiProvider and QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { WagmiProvider } from 'wagmi';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.tsx';
-import { config } from './config/reown';
 import './index.css';
-
-// Setup queryClient
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </WagmiProvider>
+    <App />
   </StrictMode>
 );
